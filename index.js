a56de960164fbc816e8a7820f0ddfdcc66cfc004
Hikmahx <hikmayousuph@gmail.com>
2023-09-06 03:35:15 +0100

    set up

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql")
+const schema = require('./schema/schema')
+dotenv.config({ path: "./config/config.env" });
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
+
+// CORS
+app.use(cors());
+
+// ROUTES
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development',
+}));
+
+
+
+app.listen(PORT, () => console.log("This is listening on PORT: " + PORT));
