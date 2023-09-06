48fd3ea5c358d45ee78b50c2468042bad9477f20
Hikmahx <hikmayousuph@gmail.com>
2023-09-06 04:08:13 +0100

    project and client

---
+++ sampleData.js
@@ -0,0 +1,72 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada quam ut libero bibendum, non scelerisque justo pharetra. Suspendisse potenti. Fusce euismod lacus nec sapien laoreet, nec finibus sapien egestas. Proin bibendum arcu nec diam consectetur, eu hendrerit mi euismod. Nunc gravida risus ac augue fermentum, sit amet efficitur justo pharetra.',
+    status: 'In progress'
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada quam ut libero bibendum, non scelerisque justo pharetra. Suspendisse potenti. Fusce euismod lacus nec sapien laoreet, nec finibus sapien egestas. Proin bibendum arcu nec diam consectetur, eu hendrerit mi euismod. Nunc gravida risus ac augue fermentum, sit amet efficitur justo pharetra.',
+    status: 'In progress'
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada quam ut libero bibendum, non scelerisque justo pharetra. Suspendisse potenti. Fusce euismod lacus nec sapien laoreet, nec finibus sapien egestas. Proin bibendum arcu nec diam consectetur, eu hendrerit mi euismod. Nunc gravida risus ac augue fermentum, sit amet efficitur justo pharetra.',
+    status: 'In progress'
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada quam ut libero bibendum, non scelerisque justo pharetra. Suspendisse potenti. Fusce euismod lacus nec sapien laoreet, nec finibus sapien egestas. Proin bibendum arcu nec diam consectetur, eu hendrerit mi euismod. Nunc gravida risus ac augue fermentum, sit amet efficitur justo pharetra.',
+    status: 'In progress'
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada quam ut libero bibendum, non scelerisque justo pharetra. Suspendisse potenti. Fusce euismod lacus nec sapien laoreet, nec finibus sapien egestas. Proin bibendum arcu nec diam consectetur, eu hendrerit mi euismod. Nunc gravida risus ac augue fermentum, sit amet efficitur justo pharetra.',
+    status: 'In progress'
+  },
+]
+
+const clients = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'ironman@gmail.com',
+    phone: '232-324-2342'
+  },
+  {
+    id: '2',
+    name: 'Natasha Romonova',
+    email: 'blackwidow@gmail.com',
+    phone: '111-314-3342'
+  },
+  {
+    id: '3',
+    name: 'Thor Odinson',
+    email: 'thor@gmail.com',
+    phone: '232-224-2355'
+  },
+  {
+    id: '4',
+    name: 'Steve Rogers',
+    email: 'steve@gmail.com',
+    phone: '666-324-2242'
+  },
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'hulk@gmail.com',
+    phone: '232-324-2342'
+  },
+]
+
+module.exports = { projects, clients }
